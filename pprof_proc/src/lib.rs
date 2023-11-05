8cd09fd13f7437edf47df737c7e4a8077501afc0
Paul Page <pjpage98@gmail.com>
2023-11-05 11:59:59 -0600

    Add feature to turn block-level profiling on or off

--- pprof_proc/src/lib.rs
+++ pprof_proc/src/lib.rs
@@ -1,6 +1,13 @@
 use proc_macro::TokenStream;
 
+#[cfg(feature = "profile")]
 #[proc_macro]
 pub fn time(item: TokenStream) -> TokenStream {
     format!("let _p = pprof::block!({});", item).parse().unwrap()
+}
+
+#[cfg(not(feature = "profile"))]
+#[proc_macro]
+pub fn time(item: TokenStream) -> TokenStream {
+    TokenStream::default()
 }
